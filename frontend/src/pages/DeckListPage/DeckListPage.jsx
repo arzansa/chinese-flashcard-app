1d23fcd2db882d03848bf46f4940587c24d2754f
arzansa <arzansa@gmail.com>
2024-09-19 12:44:56 -0500

    Fix DeckListPage to filter decks

--- frontend/src/pages/DeckListPage/DeckListPage.jsx
+++ frontend/src/pages/DeckListPage/DeckListPage.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 
 export default function DeckListPage({ decks, user }) {
+  const userDecks = decks.filter((deck) => deck.creator._id === user._id);
+
   return (
     <main>
-      <h1>Deck List Page</h1>
+      <h1>My Decks</h1>
       <Link to="/decks/new">
         <button>Create New Deck</button>
       </Link>
-      {decks.length > 0 ? (
-        decks.map((deck) => (
+      {userDecks.length > 0 ? (
+        userDecks.map((deck) => (
           <article key={deck._id}>
             <header>
               <h2>{deck.title}</h2>
